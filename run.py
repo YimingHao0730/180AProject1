6324d2badbb541c6196bd7342b04b9d73383ade2
YimingHao0730 <97846459+YimingHao0730@users.noreply.github.com>
2023-12-07 19:35:21 -0800

    Update run.py

--- run.py
+++ run.py
@@ -3,29 +3,29 @@ import os
 
 
 def data_processing():
-    if not os.path.exists(r'processed_data\combined.txt'):
-        os.system(r'type data\AMPs.fa data\Non-AMPs.fa > processed_data\combined.txt')
-        os.system(r'perl scripts\format.pl processed_data\combined.txt none > processed_data\processed_data.txt')
+    if not os.path.exists('processed_data/combined.txt'):
+        os.system('type Data/AMPs.fa Data/Non-AMPs.fa > processed_data/combined.txt')
+        os.system('perl scripts/format.pl processed_data/combined.txt none > processed_data/processed_data.txt')
     print("Data processing done")
 
 def Prediction_Attention():
-    if not os.path.exists(r"results\outcome_Attention.txt"):
-        os.system(r'python scripts\prediction_attention.py processed_data\processed_data.txt  results\outcome_Attention.txt')
+    if not os.path.exists("results/outcome_Attention.txt"):
+        os.system('python scripts/prediction_attention.py processed_data/processed_data.txt  results/outcome_Attention.txt')
     print("Prediction with Attention model complete")
 
 def Prediction_LSTM():
-    if not os.path.exists(r"results\outcome_LSTM.txt"):
-        os.system(r'python scripts\prediction_lstm.py processed_data\processed_data.txt  results\outcome_LSTM.txt')
+    if not os.path.exists("results/outcome_LSTM.txt"):
+        os.system('python scripts/prediction_lstm.py processed_data/processed_data.txt  results/outcome_LSTM.txt')
     print("Prediction with LSTM model complete")
 
 def Prediction_Bert():
-    if not os.path.exists(r"results\outcome_Bert.txt"):
-        os.system(r'python scripts\prediction_bert.py processed_data\combined.txt  results\outcome_Bert.txt')
+    if not os.path.exists("results/outcome_Bert.txt"):
+        os.system('python scripts/prediction_bert.py processed_data/combined.txt  results/outcome_Bert.txt')
     print("Prediction with Bert model complete")
 
 def Result_integration():
-    os.system(r'python scripts\result.py results\outcome_Attention.txt results\outcome_LSTM.txt results\outcome_Bert.txt processed_data\combined.txt results\combined_outcome.csv')
-    os.system(r'python scripts\final_results.py')
+    os.system('python scripts/result.py results/outcome_Attention.txt results/outcome_LSTM.txt results/outcome_Bert.txt processed_data/combined.txt results/combined_outcome.csv')
+    os.system('python scripts/final_results.py')
     print('result integration complete')
 
 if __name__ == "__main__":
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     if argument == "data":
         data_processing()
     elif argument == "prediction":
-        if not os.path.exists(r"processed_data\processed_data.txt"):
+        if not os.path.exists("processed_data/processed_data.txt"):
             raise FileNotFoundError("No processed data. Please run 'python run.py data' first.")
         else:
             Prediction_Attention()

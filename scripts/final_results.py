4d4f18b56a6365313313fe8a308881c311af9216
YimingHao0730 <97846459+YimingHao0730@users.noreply.github.com>
2023-12-08 15:44:51 -0800

    updated results

--- scripts/final_results.py
+++ scripts/final_results.py
@@ -4,24 +4,19 @@ from sklearn.metrics import precision_score, recall_score, confusion_matrix, pre
 # Read the CSV file
 df = pd.read_csv('results/combined_outcome.csv', sep='\t')
 
-
 # Add the 'actual' column
 df['actual'] = [1 if i < 1085 else 0 for i in range(len(df))]
 
-# Calculate TP, FP, TN, FN
-tp = sum((df['prediction'] == 1) & (df['actual'] == 1))
-fp = sum((df['prediction'] == 1) & (df['actual'] == 0))
-tn = sum((df['prediction'] == 0) & (df['actual'] == 0))
-fn = sum((df['prediction'] == 0) & (df['actual'] == 1))
+# Define models with their respective prediction and probability columns
+models = {
+    'Attention+LSTM+Bert': ('prediction', 'probability'),
+    'Attention': ('att_prediction', 'att_prob'),
+    'LSTM': ('lstm_prediction', 'lstm_prob'),
+    'Bert': ('bert_prediction', 'bert_prob')
+}
 
-# Calculate AUPRC, Precision, Recall, FNR
-precision = precision_score(df['actual'], df['prediction'])
-recall = recall_score(df['actual'], df['prediction'])
-fnr = fn / (fn + tp)
-precision1, recall1, _ = precision_recall_curve(df['actual'], df['probability'])
-auprc = auc(recall1, precision1)
-# Prepare HTML table
-html = f"""
+# Start HTML table
+html = """
 <html>
 <head><title>Sequences ≤ 50 AAs</title></head>
 <body>
@@ -37,8 +32,27 @@ html = f"""
             <th>Recall</th>
             <th>FNR</th>
         </tr>
+"""
+
+# Loop through each model and calculate metrics
+for model, (prediction_col, prob_col) in models.items():
+    # Calculate TP, FP, TN, FN
+    tp = sum((df[prediction_col] == 1) & (df['actual'] == 1))
+    fp = sum((df[prediction_col] == 1) & (df['actual'] == 0))
+    tn = sum((df[prediction_col] == 0) & (df['actual'] == 0))
+    fn = sum((df[prediction_col] == 0) & (df['actual'] == 1))
+
+    # Calculate AUPRC, Precision, Recall, FNR
+    precision = precision_score(df['actual'], df[prediction_col])
+    recall = recall_score(df['actual'], df[prediction_col])
+    fnr = fn / (fn + tp)
+    precision_curve, recall_curve, _ = precision_recall_curve(df['actual'], df[prob_col])
+    auprc = auc(recall_curve, precision_curve)
+
+    # Append to HTML
+    html += f"""
         <tr>
-            <td>Attention+LSTM+Bert</td>
+            <td>{model}</td>
             <td>{tp}</td>
             <td>{fp}</td>
             <td>{tn}</td>
@@ -48,6 +62,10 @@ html = f"""
             <td>{recall:.4f}</td>
             <td>{fnr:.4f}</td>
         </tr>
+    """
+
+# Close HTML table
+html += """
     </table>
 </body>
 </html>
--- scripts/result.py
+++ scripts/result.py
@@ -14,7 +14,13 @@ result_output = sys.argv[5]
 # Read in the prediction files and store the results in a hash table
 # with the line number as the key and the prediction as the value
 results = {}
+results_att = {}
+results_lstm = {}
+results_bert = {}
 probabilities = {}
+probabilities_att = {}
+probabilities_lstm = {}
+probabilities_bert = {}
 
 # Read in att_bact.txt
 with open(att_bact_file, 'r') as f:
@@ -23,9 +29,13 @@ with open(att_bact_file, 'r') as f:
         if float(line) > 0.5:
             results[i] = 1
             probabilities[i] = float(line)
+            results_att[i] = 1
+            probabilities_att[i] = float(line)
         else:
             results[i] = 0
             probabilities[i] = 0
+            results_att[i] = 0
+            probabilities_att[i] = 0
 
 # Read in lstm_bact.txt
 with open(lstm_bact_file, 'r') as f:
@@ -34,9 +44,13 @@ with open(lstm_bact_file, 'r') as f:
         if float(line) > 0.5:
             results[i] += 1
             probabilities[i] += float(line)
+            results_lstm[i] = 1
+            probabilities_lstm[i] = float(line)
         else:
             results[i] += 0
             probabilities[i] += 0
+            results_lstm[i] = 0
+            probabilities_lstm[i] = 0
 
 # Read in bert_bact.txt
 with open(bert_bact_file, 'r') as f:
@@ -45,9 +59,13 @@ with open(bert_bact_file, 'r') as f:
         if float(line) > 0.5:
             results[i] += 1
             probabilities[i] += float(line)
+            results_bert[i] = 1
+            probabilities_bert[i] = float(line)
         else:
             results[i] += 0
             probabilities[i] += 0
+            results_bert[i] = 0
+            probabilities_bert[i] = 0
 
 # Print the number of sequences that were processed
 print("There are {} sequences need to predictive.".format(len(results)))
@@ -64,6 +82,12 @@ with open(sequence_file, 'r') as f:
             seq = data_ls[idx + 1].strip()
             length = len(seq)
             prediction = results[idx/2]
+            att_prediction = results_att[idx/2]
+            lstm_prediction = results_lstm[idx/2]
+            bert_prediction = results_bert[idx/2]
+            att_prob = probabilities_att[idx/2]
+            lstm_prob = probabilities_lstm[idx/2]
+            bert_prob = probabilities_bert[idx/2]
             probability = probabilities[idx/2]/3
             if prediction == 3:
                 prediction = 1
@@ -74,7 +98,13 @@ with open(sequence_file, 'r') as f:
                 "seq": seq,
                 "length": length,
                 "prediction": prediction,
-                'probability': probability
+                'probability': probability,
+                'att_prediction': att_prediction,
+                'lstm_prediction': lstm_prediction,
+                'bert_prediction': bert_prediction,
+                'att_prob': att_prob,
+                'lstm_prob': lstm_prob,
+                'bert_prob': bert_prob
             })
 
 pd.DataFrame(res_ls).to_csv(result_output, sep='\t', index=False)
